6f67f27aa29b54a0ffab89a2880f031367ceef7a
NardosYosef <nardosyosef123@gmail.com>
2024-04-07 09:37:27 +0300

    fix

--- models/event.js
+++ models/event.js
@@ -6,14 +6,12 @@ const eventSchema = new mongoose.Schema({
     date: {type: Date ,unique:false, required: true},
     time:{type: String ,unique:false, required: true},
     // rate: {type: Number ,unique:false, required: true},
-    price: {type: Number ,unique:false, required: true},
+    price: {type: Number ,unique:false, required: false},
     availableTickets: {type: Number ,unique:false, required: true},
     image: {type: String ,unique:false, required: true},
     eventOrganizer: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     status: {type: String ,unique:false, required: false},
-  
-
   });
   const Event = mongoose.model('Event', eventSchema);
   module.exports=Event
