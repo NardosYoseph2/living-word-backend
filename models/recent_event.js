5d683b05988875e9bb36f8b342375487f82d9e25
NardosYoseph <nardosyosef123@gmail.com>
2024-06-07 11:57:18 +0300

    recent event

--- models/recent_event.js
+++ models/recent_event.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const recentEventSchema = new mongoose.Schema({
     title: {type: String ,unique:false, required: true},
     description: {type: String ,unique:false, required: false},
-    image: {type: String ,unique:false, required: true},
+    thumbnail: {type: String ,unique:false, required: true},
     video: {type: String ,unique:false, required: true},
   });
   const RecentEvent = mongoose.model('RecentEvent', recentEventSchema);
